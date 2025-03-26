c8ff60d59917df418cfbee1fe0d2cce79c768193
Sam Hart <81168131+tofuprogrammer@users.noreply.github.com>
2025-03-26 13:35:00 +0000

    Initial Commit

---
+++ Bet365Hackathon/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Bet365Hackathon
+//
+//  Created by Sam Hart on 26/03/2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
